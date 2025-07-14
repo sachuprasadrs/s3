325f80c67433363c854249946798d4d7d783743b
sachuprasadrs <sachuprasadrs@gmail.com>
2025-07-14 13:44:56 +0530

    Create 06transpose.py

---
+++ DataScience LAB/06transpose.py
@@ -0,0 +1,11 @@
+import numpy as np
+n=int(input("Enter size of row (n): "))
+m=int(input("Enter size of column (m): "))
+print(f"Enter {n*m} elements (row-wise, space separated):")
+elements = list(map(float, input().split()))
+matrix = np.array(elements).reshape(n, m)
+print("\nOriginal Matrix:")
+print(matrix)
+transpose = matrix.T
+print("\nTranspose of the Matrix:")
+print(transpose)
