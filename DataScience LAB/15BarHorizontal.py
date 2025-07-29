fd09f4141e4fe1e7672f647da71f30106a2ac529
sachuprasadrs <sachuprasadrs@gmail.com>
2025-07-29 14:42:25 +0530

    Create 15BarHorizontal.py

---
+++ DataScience LAB/15BarHorizontal.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.array(['Java','Python','PHP','Javascript','C#','C++'])
+y=np.array([22.2,17.6,8.8,8,7.7,6.7])
+colors=['#b07219', '#3572A5','#8892be','#f1e05a', '#178600','blue']
+plt.barh(x,y,color=colors)
+plt.title('Bar Chart')
+plt.xlabel('Programming Language')
+plt.ylabel('Popularity')
+plt.show()
