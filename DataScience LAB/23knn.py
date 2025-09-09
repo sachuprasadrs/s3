434de3284ee074fa1fd93209086f19e77ca37fc5
sachuprasadrs <sachuprasadrs@gmail.com>
2025-09-09 14:52:12 +0530

    Update 23knn.py

--- DataScience LAB/23knn.py
+++ DataScience LAB/23knn.py
@@ -5,8 +5,8 @@ from sklearn.neighbors import KNeighborsClassifier
 df = pd.read_csv('tshirt.csv')
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1]
-print(X)
-print(y)
+row1=df.iloc[1:]
+print(row1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
@@ -14,7 +14,7 @@ y_pred = knn.predict(X_test)
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 h = int(input('Enter height: '))
 w = int(input('Enter weight: '))
-samples = pd.DataFrame({'height': [h], 'weight': [w]})
+samples = pd.DataFrame({'Height': [h], 'Weight': [w]})
 
 pred_v = knn.predict(samples)
 print("Predicted class:", pred_v[0])
