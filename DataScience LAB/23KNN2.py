5dcd7f5b1c4e631a93a8af595f4e44b2ac698b12
sachuprasadrs <sachuprasadrs@gmail.com>
2025-09-09 14:26:10 +0530

    Update and rename 23KNN.py to 23KNN2.py

--- DataScience LAB/23KNN2.py
+++ DataScience LAB/23KNN2.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+
 df = pd.read_csv('tshirt.csv')
 X = df[['Height', 'Weight']]
 y = df['T_Shirt_Size']
-
+#row1=df.iloc[1:]
+#print(row1)
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X, y)
-a=int(input(("Enter height"))
-b=int(input(("Enter weight"))
-sample = [[a,b]]
+a = int(input("Enter height: "))
+b = int(input("Enter weight: "))
+sample = [[a, b]]
 predicted_size = knn.predict(sample)
 print(f"The predicted t-shirt size is: {predicted_size[0]}")
